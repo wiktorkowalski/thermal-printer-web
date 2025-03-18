a0daa8a1a67590c009da962e22fd8bdabff19b8a
wiktorkowalski <wiktor9196667@gmail.com>
2025-03-18 22:23:53 +0100

    chore: removed newrelic tooling

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import newrelic from 'newrelic';
 import path from 'path';
 
 const __dirname = new URL('.', import.meta.url).pathname;
@@ -10,7 +9,7 @@ const nextConfig = {
     // Without this setting, the Next.js compilation step will routinely
     // try to import files such as `LICENSE` from the `newrelic` module.
     // See https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages.
-    serverComponentsExternalPackages: ['newrelic']
+    // serverComponentsExternalPackages: ['newrelic']
   },
   webpack: (config) => {
     config.resolve.alias = {
@@ -21,9 +20,5 @@ const nextConfig = {
     return config;
   },
 };
-
-// console.log('Starting thermal-printer-web');
-// newrelic.incrementMetric('thermal-printer-web.start');
-// console.log('Started thermal-printer-web');
 
 export default nextConfig;
